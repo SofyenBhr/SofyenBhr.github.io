7f0264be96a504e8ed3a4f2ea69e1a825684c7c1
SofyenBhr <sofyenbhr@gmail.com>
2022-10-15 16:55:44 +0100

    add the header and its style

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,1 +1,7 @@
-document.querySelector("h1").style.color = "red";
+document.querySelector("#burger-menu").addEventListener("click", () => {
+    if (document.querySelector("#menu").style.display === "block") {
+        document.querySelector("#menu").style.display = "none";
+    } else {
+        document.querySelector("#menu").style.display = "block";
+    }
+});
