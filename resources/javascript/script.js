5fc0196179f74243e6854eb1742bf9563abdf5a7
SofyenBhr <sofyenbhr@gmail.com>
2022-10-16 14:07:14 +0100

    fix the overlay

--- resources/javascript/script.js
+++ resources/javascript/script.js
@@ -1,9 +1,9 @@
 document.querySelector("#burger-menu").addEventListener("click", () => {
     if (document.querySelector("#menu").style.display === "none") {
         document.querySelector("#menu").style.display = "block";
-        document.querySelector("#banner-overlay").style.top = "13rem";
+        document.querySelector("#banner-overlay").style.top = "209px";
     } else {
         document.querySelector("#menu").style.display = "none";
-        document.querySelector("#banner-overlay").style.top = "3.4rem";
+        document.querySelector("#banner-overlay").style.top = "54px";
     }
 });
